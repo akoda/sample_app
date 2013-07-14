de50728063dd54deb2b85c0df7c89e9a3e2eee1d
Alan Koda <alankoda@gmail.com>
2013-07-13 19:47:27 -0500

    Finish user signup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -45,6 +45,16 @@ describe User do
     it { should_not be_valid }
   end
   
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+    
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
+  end
+  
   describe "when email format is invalid" do
       it "should be invalid" do
         addresses = %w[user@foo,com user_at_foo.org example.user@foo.
@@ -110,6 +120,33 @@ describe User do
         it { should_not == user_for_invalid_password }
         specify { user_for_invalid_password.should be_false }
       end
+    end
+    
+    describe "signup" do
+      
+      before { visit signup_path }
+      
+      let(:submit) { "Create my account" }
+      
+      describe "with invalid information" do
+        it "should not create a user" do
+          expect { click_button :submit }.not_to change(User, :count)
+        end
+      end
+      
+      describe "with valid information" do
+        before do
+          fill_in "Name",         with: "Example User"
+          fill_in "Email",        with: "user@example.com"
+          fill_in "Password",     with: "foobar"
+          fill_in "Confirmation", with: "foobar"
+        end
+        
+        it "should create a user" do
+          expect { click_button :submit }.to change(User, :count).by(1)
+        end
+      end
+      
     end
 
 end
